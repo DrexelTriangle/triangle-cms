6d17f2c5b45d905f09b835f6f3c3e4d35563af09
ssavutu <ssavutu@gmail.com>
2026-02-26 00:31:24 -0500

    Update handler to use json response

--- server/internal/handlers/handlers.go
+++ server/internal/handlers/handlers.go
@@ -1,7 +1,29 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+)
+
+type ResponsePayload struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+}
 
 func Users(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("/users response"))
+	w.Header().Set("Content-Type", "application/json")
+
+	payload := ResponsePayload{
+		Status:  "OK",
+		Message: "Users endpoint hit",
+		Code:    http.StatusOK,
+	}
+
+	err := json.NewEncoder(w).Encode(payload)
+	if err != nil {
+		slog.Error("error encoding json: %v\n", "error", err)
+		http.Error(w, "500 - Internal Server Error", http.StatusInternalServerError)
+	}
 }
