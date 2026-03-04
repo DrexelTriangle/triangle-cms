dff3f4b30857d93389625f141938e95f9d9498f2
Kristine <kly35@drexel.edu>
2026-03-03 19:49:17 -0500

    component organization + homepage cleanup

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,17 @@
+
+import '../App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <p className="homepage-temporary-title">
+        THIS IS THE HOMEPAGE
+      </p>
+    </>
+  )
+}
+
+export default App
