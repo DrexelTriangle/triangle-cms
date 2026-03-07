891095b521fb8b245e69b986551c1e9aebc1296b
Kristine <kly35@drexel.edu>
2026-03-06 21:27:40 -0500

    sidebar

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import '../App.css'
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar"></div>
+    )
+}
+
+export default Sidebar
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App.tsx'
 import Header from './components/header.tsx'
+import Sidebar from './components/Sidebar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+
     <App />
     <Header />
+    <Sidebar />
+
   </StrictMode>,
 )
